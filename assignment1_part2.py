96344e48df886dff26b3926aded31ee19789c8f4
w0rdsandstuff <77218212+w0rdsandstuff@users.noreply.github.com>
2021-09-03 08:27:51 -0400

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,22 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
-    
+        """
+        Constructor of a Book class
+        :param name: The name of the author
+        :param team: The title of the book
+        """
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        """ Display the author and book title"""
+        print(f'{self.title} written by {self.author} ')
 
 
 if __name__ == "__main__":
-    pass
+    a1 = Book('John Steinbeck', 'Of Mice and Men')
+    a2 = Book('Harper Lee', "To Kill a Mockingbird")
+
+    a1.display()
+    a2.display()
